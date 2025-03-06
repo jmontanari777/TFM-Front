1bd1d7eac7e2160aa00f60c92b81495458ff4755
jmontanari777 <jmontanari@outlook.com>
2025-03-06 22:51:12 +0100

    Borre todo lo de .ENV

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-require('dotenv').config()
+
 
 //const SOCKET_SERVER_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
 const SOCKET_SERVER_URL = 'http://localhost:3000'
